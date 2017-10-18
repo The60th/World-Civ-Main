1880a5fdb47464b86ecb9d3366fc75591a58f2c0
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2017-10-18 12:21:30 -0700

    fixing pushed errors

--- src/com/worldciv/events/player/anvilCreate.java
+++ src/com/worldciv/events/player/anvilCreate.java
@@ -1,8 +1,8 @@
 package com.worldciv.events.player;
 
-import com.google.common.primitives.Bytes;
-import com.mysql.fabric.xmlrpc.base.Array;
-import org.bukkit.*;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
@@ -16,8 +16,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 
 import static com.worldciv.utility.utilityStrings.worldciv;
@@ -113,7 +111,9 @@ public class anvilCreate implements Listener {
         }
 
         //This is where all the functions for anvil recipes go. Not actual click event!
-        dummyRecipeItem(itemsInAnvil, e);
+        RecipeRefinedIronIngot(itemsInAnvil, e);
+        RecipeCokeFuel(itemsInAnvil, e);
+        RecipeActivatedCarbon(itemsInAnvil, e);
         dummyLoreRecipeItem(itemsInAnvil, e);
 
     }
@@ -137,7 +137,9 @@ public class anvilCreate implements Listener {
                     }
 
                     //This is where all the functions for item clicking goes. Not actual recipe display!
-                    dummyClickItem(p, itemsInAnvil, e); //Example Item To Click And Receive
+                    ClickRefinedIronIngot(p, itemsInAnvil, e); //Example Item To Click And Receive
+                    ClickCokeFuel(p, itemsInAnvil, e);
+                    ClickActivatedCarbon(p, itemsInAnvil, e);
                     dummyLoreClickItem(p, itemsInAnvil, e);
 
                 }
@@ -145,28 +147,29 @@ public class anvilCreate implements Listener {
         }
     }
 
-    private void dummyClickItem(HumanEntity p, ItemStack[] itemsInAnvil, InventoryClickEvent e) {  // Dummy Item Click Event: Material + Material = ItemStack
+    private void ClickRefinedIronIngot(HumanEntity p, ItemStack[] itemsInAnvil, InventoryClickEvent e) {  // Dummy Item Click Event: Material + Material = ItemStack
 
-        Material firstitem = Material.DIRT;
-        Material seconditem = Material.SAND;
+        Material firstitem = Material.IRON_INGOT;
+        Material seconditem = Material.IRON_INGOT;
 
-        ItemStack resultitem = new ItemStack(Material.EGG, 1); //dummy item
+        ItemStack resultitem = new ItemStack(Material.IRON_INGOT, 1); //dummy item
         ItemMeta resultitemmeta = resultitem.getItemMeta();
-        resultitemmeta.setLore(Arrays.asList("imisspetra"));
-        resultitemmeta.setUnbreakable(true);
+        resultitemmeta.setLore(Arrays.asList(ChatColor.GRAY + "The refined iron ingot is the", ChatColor.GRAY + "core ingredient to create equipment.", ChatColor.GRAY + "It is also the base material to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitemmeta.setDisplayName(ChatColor.GRAY + "Refined Iron Ingot");
         resultitem.setItemMeta(resultitemmeta);
 
-        ItemStack resultitem5 = new ItemStack(Material.EGG, 5); //dummy item as 5, don't change 5, some stuff with 5 is hardcoded. i'd keep as 5 since multiples of 5 are really nice.
+        ItemStack resultitem5 = new ItemStack(Material.IRON_INGOT, 5); //dummy item as 5, don't change 5, some stuff with 5 is hardcoded. i'd keep as 5 since multiples of 5 are really nice.
         ItemMeta resultitem5meta = resultitem5.getItemMeta();
-        resultitem5meta.setLore(Arrays.asList("imisspetra"));
-        resultitem5meta.setUnbreakable(true);
+        resultitem5meta.setLore(Arrays.asList(ChatColor.GRAY + "The refined iron ingot is the", ChatColor.GRAY + "core ingredient to create equipment.", ChatColor.GRAY + "It is also the base material to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitem5meta.setDisplayName(ChatColor.GRAY + "Refined Iron Ingot");
         resultitem5.setItemMeta(resultitem5meta);
 
-        String resultname = "enchanted egg(s)!"; //add exclamation mark and plural cases, this is what it prints: You got "x" resultname
+        String resultname = "refined iron ingot(s)!"; //add exclamation mark and plural cases, this is what it prints: You got "x" resultname
 
 
         if (itemsInAnvil[0].getType() == firstitem && itemsInAnvil[1].getType() == seconditem) //If first slot is that and if second slot is that
         {
+
             Block anvil = e.getInventory().getLocation().getBlock();
 
 
@@ -228,15 +231,191 @@ public class anvilCreate implements Listener {
 
     }
 
-    private void dummyRecipeItem(ItemStack[] itemsInAnvil, PrepareAnvilEvent e) { //Dummy Recipe: Material + Material = Item Stack (DISPLAYONLY)
+    private void ClickCokeFuel(HumanEntity p, ItemStack[] itemsInAnvil, InventoryClickEvent e) {  // Dummy Item Click Event: Material + Material = ItemStack
 
-        Material firstitem = Material.DIRT;
-        Material seconditem = Material.SAND;
+        Material firstitem = Material.COAL;
+        Material seconditem = Material.COAL;
 
-        ItemStack resultitem = new ItemStack(Material.EGG, 1); //dummy item
+        ItemStack resultitem = new ItemStack(Material.COAL, 1); //dummy item
         ItemMeta resultitemmeta = resultitem.getItemMeta();
-        resultitemmeta.setLore(Arrays.asList("imisspetra"));
-        resultitemmeta.setUnbreakable(true);
+        resultitemmeta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "Coke is a fuel with no", ChatColor.DARK_GRAY + "impurities and high burn time.", ChatColor.DARK_GRAY + "It is also the best fuel to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitemmeta.setDisplayName(ChatColor.DARK_GRAY + "Coke (fuel)");
+        resultitem.setItemMeta(resultitemmeta);
+
+        ItemStack resultitem5 = new ItemStack(Material.COAL, 5); //dummy item as 5, don't change 5, some stuff with 5 is hardcoded. i'd keep as 5 since multiples of 5 are really nice.
+        ItemMeta resultitem5meta = resultitem5.getItemMeta();
+        resultitem5meta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "Coke is a fuel with no", ChatColor.DARK_GRAY + "impurities and high burn time.", ChatColor.DARK_GRAY + "It is also the best fuel to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitem5meta.setDisplayName(ChatColor.DARK_GRAY + "Coke (fuel)");
+        resultitem5.setItemMeta(resultitem5meta);
+
+        String resultname = "coke(s)!"; //add exclamation mark and plural cases, this is what it prints: You got "x" resultname
+
+
+        if (itemsInAnvil[0].getType() == firstitem && itemsInAnvil[1].getType() == seconditem) //If first slot is that and if second slot is that
+        {
+            if(itemsInAnvil[0].getDurability() != 1 && itemsInAnvil[1].getDurability() != 1){ //this is coal coal from underground  *NOT* charcoal
+
+            Block anvil = e.getInventory().getLocation().getBlock();
+
+
+            if (e.isShiftClick() && ((p.getInventory().firstEmpty() != -1) || !isAllFull(p, resultitem5))) {  //checking if shift click and is not full for 5!
+
+
+                if (itemsInAnvil[0].getAmount() < 5 || itemsInAnvil[1].getAmount() < 5) { //When you dont have 5 of the required items! in either slot!
+                    p.sendMessage(worldciv + ChatColor.GRAY + " Not enough material to buy " + ChatColor.YELLOW + "5 " + resultname);
+                    return;
+                }
+
+                p.sendMessage(worldciv + ChatColor.GRAY + " You have added " + ChatColor.YELLOW + "5 " + ChatColor.GRAY + resultname);
+
+                if (itemsInAnvil[0].getAmount() == 5) { //check for first slot has 5 items
+                    itemsInAnvil[0].setAmount(-1); //remove them to emptiness
+                }
+
+                if (itemsInAnvil[1].getAmount() == 5) { //same as above
+                    itemsInAnvil[1].setAmount(-1);
+                }
+
+                anvilBreakShiftLeft(anvil, p);
+
+                itemsInAnvil[0].setAmount(itemsInAnvil[0].getAmount() - 5); //Removes 5 every time! hype! Once you have emptiness these lines dont matter. the more negative the more emptiness!
+                itemsInAnvil[1].setAmount(itemsInAnvil[1].getAmount() - 5);
+
+                p.getInventory().addItem(resultitem5); //Add that item! ohayo~!
+
+                ((Player) p).updateInventory(); //update inventory cos glitchy sometimes!
+
+            } else if (!isAllFull(p, resultitem) || (p.getInventory().firstEmpty() != -1)) { //same as the check before this. just single clicks
+
+                p.sendMessage(worldciv + ChatColor.GRAY + " You have added " + ChatColor.YELLOW + "1 " + ChatColor.GRAY + resultname);
+
+                if (itemsInAnvil[0].getAmount() == 1) { //same as stuff above with 5, no need to spam here now..
+                    itemsInAnvil[0].setAmount(-1);
+                }
+
+                if (itemsInAnvil[1].getAmount() == 1) {
+                    itemsInAnvil[1].setAmount(-1);
+                }
+
+
+                anvilBreakSingleClick(anvil, p);
+                itemsInAnvil[0].setAmount(itemsInAnvil[0].getAmount() - 1);
+                itemsInAnvil[1].setAmount(itemsInAnvil[1].getAmount() - 1);
+
+
+                p.getInventory().addItem(resultitem);
+
+                ((Player) p).updateInventory();
+
+
+            } else {
+
+                p.sendMessage(worldciv + ChatColor.GRAY + " Inventory is full! You can't forge this!"); //no inv space :)
+
+            }
+        }
+        }
+
+    }
+
+    private void ClickActivatedCarbon(HumanEntity p, ItemStack[] itemsInAnvil, InventoryClickEvent e) {  // Dummy Item Click Event: Material + Material = ItemStack
+
+        Material firstitem = Material.COAL;
+        Material seconditem = Material.COAL;
+
+        ItemStack resultitem = new ItemStack(Material.COAL, 1); //dummy item
+        resultitem.setDurability((short) 1);
+        ItemMeta resultitemmeta = resultitem.getItemMeta();
+        resultitemmeta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "Activated Carbon is a fuel", ChatColor.DARK_GRAY + "with medium-high burn time.", ChatColor.DARK_GRAY + "It is cheap fuel to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitemmeta.setDisplayName(ChatColor.DARK_GRAY + "Activated Carbon");
+        resultitem.setItemMeta(resultitemmeta);
+
+        ItemStack resultitem5 = new ItemStack(Material.COAL, 5); //dummy item as 5, don't change 5, some stuff with 5 is hardcoded. i'd keep as 5 since multiples of 5 are really nice.
+        resultitem5.setDurability((short) 1);
+        ItemMeta resultitem5meta = resultitem5.getItemMeta();
+        resultitem5meta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "Activated Carbon is a fuel", ChatColor.DARK_GRAY + "with medium-high burn time.", ChatColor.DARK_GRAY + "It is cheap fuel to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitem5meta.setDisplayName(ChatColor.DARK_GRAY + "Activated Carbon");
+        resultitem5.setItemMeta(resultitem5meta);
+
+        String resultname = "Activated Carbon(s)!"; //add exclamation mark and plural cases, this is what it prints: You got "x" resultname
+
+
+        if (itemsInAnvil[0].getType() == firstitem && itemsInAnvil[1].getType() == seconditem) //If first slot is that and if second slot is that
+        {
+            if(itemsInAnvil[0].getDurability() == 1 && itemsInAnvil[1].getDurability() == 1){ //this is coal coal from underground  *NOT* charcoal
+
+                Block anvil = e.getInventory().getLocation().getBlock();
+
+
+                if (e.isShiftClick() && ((p.getInventory().firstEmpty() != -1) || !isAllFull(p, resultitem5))) {  //checking if shift click and is not full for 5!
+
+
+                    if (itemsInAnvil[0].getAmount() < 5 || itemsInAnvil[1].getAmount() < 5) { //When you dont have 5 of the required items! in either slot!
+                        p.sendMessage(worldciv + ChatColor.GRAY + " Not enough material to buy " + ChatColor.YELLOW + "5 " + resultname);
+                        return;
+                    }
+
+                    p.sendMessage(worldciv + ChatColor.GRAY + " You have added " + ChatColor.YELLOW + "5 " + ChatColor.GRAY + resultname);
+
+                    if (itemsInAnvil[0].getAmount() == 5) { //check for first slot has 5 items
+                        itemsInAnvil[0].setAmount(-1); //remove them to emptiness
+                    }
+
+                    if (itemsInAnvil[1].getAmount() == 5) { //same as above
+                        itemsInAnvil[1].setAmount(-1);
+                    }
+
+                    anvilBreakShiftLeft(anvil, p);
+
+                    itemsInAnvil[0].setAmount(itemsInAnvil[0].getAmount() - 5); //Removes 5 every time! hype! Once you have emptiness these lines dont matter. the more negative the more emptiness!
+                    itemsInAnvil[1].setAmount(itemsInAnvil[1].getAmount() - 5);
+
+                    p.getInventory().addItem(resultitem5); //Add that item! ohayo~!
+
+                    ((Player) p).updateInventory(); //update inventory cos glitchy sometimes!
+
+                } else if (!isAllFull(p, resultitem) || (p.getInventory().firstEmpty() != -1)) { //same as the check before this. just single clicks
+
+                    p.sendMessage(worldciv + ChatColor.GRAY + " You have added " + ChatColor.YELLOW + "1 " + ChatColor.GRAY + resultname);
+
+                    if (itemsInAnvil[0].getAmount() == 1) { //same as stuff above with 5, no need to spam here now..
+                        itemsInAnvil[0].setAmount(-1);
+                    }
+
+                    if (itemsInAnvil[1].getAmount() == 1) {
+                        itemsInAnvil[1].setAmount(-1);
+                    }
+
+
+                    anvilBreakSingleClick(anvil, p);
+                    itemsInAnvil[0].setAmount(itemsInAnvil[0].getAmount() - 1);
+                    itemsInAnvil[1].setAmount(itemsInAnvil[1].getAmount() - 1);
+
+
+                    p.getInventory().addItem(resultitem);
+
+                    ((Player) p).updateInventory();
+
+
+                } else {
+
+                    p.sendMessage(worldciv + ChatColor.GRAY + " Inventory is full! You can't forge this!"); //no inv space :)
+
+                }
+            }
+        }
+
+    }
+
+    private void RecipeRefinedIronIngot(ItemStack[] itemsInAnvil, PrepareAnvilEvent e) { //Dummy Recipe: Material + Material = Item Stack (DISPLAYONLY)
+
+        Material firstitem = Material.IRON_INGOT;
+        Material seconditem = Material.IRON_INGOT;
+
+        ItemStack resultitem = new ItemStack(Material.IRON_INGOT, 1); //dummy item
+        ItemMeta resultitemmeta = resultitem.getItemMeta();
+        resultitemmeta.setLore(Arrays.asList(ChatColor.GRAY + "The refined iron ingot is the", ChatColor.GRAY + "core ingredient to create equipment.", ChatColor.GRAY + "It is also the base material to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitemmeta.setDisplayName(ChatColor.GRAY + "Refined Iron Ingot");
         resultitem.setItemMeta(resultitemmeta);
 
 
@@ -246,6 +425,52 @@ public class anvilCreate implements Listener {
         }
 
     }
+
+    private void RecipeCokeFuel(ItemStack[] itemsInAnvil, PrepareAnvilEvent e) { //Dummy Recipe: Material + Material = Item Stack (DISPLAYONLY)
+
+        Material firstitem = Material.COAL;
+        Material seconditem = Material.COAL;
+
+        ItemStack resultitem = new ItemStack(Material.COAL, 1); //dummy item
+        ItemMeta resultitemmeta = resultitem.getItemMeta();
+        resultitemmeta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "Coke is a fuel with no", ChatColor.DARK_GRAY + "impurities and high burn time.", ChatColor.DARK_GRAY + "It is also the best fuel to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitemmeta.setDisplayName(ChatColor.DARK_GRAY + "Coke (fuel)");
+        resultitem.setItemMeta(resultitemmeta);
+
+
+        if (itemsInAnvil[0].getType() == firstitem && itemsInAnvil[1].getType() == seconditem) {  //if only stuff was as this easy..
+
+            if (itemsInAnvil[0].getDurability() != 1 && itemsInAnvil[1].getDurability() != 1) {
+            //THIS IS COAL
+                e.setResult(resultitem); //display the item
+            }
+        }
+
+    }
+
+    private void RecipeActivatedCarbon(ItemStack[] itemsInAnvil, PrepareAnvilEvent e) { //Dummy Recipe: Material + Material = Item Stack (DISPLAYONLY)
+
+        Material firstitem = Material.COAL;
+        Material seconditem = Material.COAL;
+
+        ItemStack resultitem = new ItemStack(Material.COAL, 1); //dummy item
+        resultitem.setDurability((short) 1);
+        ItemMeta resultitemmeta = resultitem.getItemMeta();
+        resultitemmeta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "Activated Carbon is a fuel", ChatColor.DARK_GRAY + "with medium-high burn time.", ChatColor.DARK_GRAY + "It is cheap fuel to create steel.", "", ChatColor.YELLOW + "Follow the guide on the /website"));
+        resultitemmeta.setDisplayName(ChatColor.DARK_GRAY + "Activated Carbon");
+        resultitem.setItemMeta(resultitemmeta);
+
+
+        if (itemsInAnvil[0].getType() == firstitem && itemsInAnvil[1].getType() == seconditem) {  //if only stuff was as this easy..
+
+            if (itemsInAnvil[0].getDurability() == 1 && itemsInAnvil[1].getDurability() == 1) {
+                //THIS IS CHARCOAL
+                e.setResult(resultitem); //display the item
+            }
+        }
+
+    }
+
 
     private void dummyLoreClickItem(HumanEntity p, ItemStack[] itemsInAnvil, InventoryClickEvent e) {  // Dummy Item Click Event: ItemStack + ItemStack = ItemStack
 
