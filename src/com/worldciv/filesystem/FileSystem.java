d7fda10c074bb917bee67513adc4aef4188c74a8
JustinFTC8397 <justinthe60th@gmail.com>
2017-12-09 11:19:35 -0500

    Fixing bug in file System

--- src/com/worldciv/filesystem/FileSystem.java
+++ src/com/worldciv/filesystem/FileSystem.java
@@ -12,58 +12,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileSystem {
-    public static boolean exists = false;
+    public static boolean itemFilesExists = false;
 
     public FileSystem(){
         File dir = new File(Bukkit.getPluginManager().getPlugin("WorldCivMaster").getDataFolder()+"/Custom_Items");
         if(!dir.exists()) {
             dir.mkdir();
-            Bukkit.broadcastMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Warning!");
+            Bukkit.broadcastMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Warning! Check console for debug data!");
             Main.logger.info(("Creating new UID System, please check all plugins if this is not wanted!"));
-            File file = new File(dir, "Custom_Items_UID_System.yml");
-            if (!(file.exists())) {
-                Bukkit.broadcastMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Warning!");
-                Bukkit.broadcastMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Creating new UID System, please check all plugins if this is not wanted!");
-                YamlConfiguration yamlFile = YamlConfiguration.loadConfiguration(file);
-                yamlFile.createSection("UID");
-
-                yamlFile.createSection("UID.customArmor");
-                yamlFile.createSection("UID.customWeapon");
-
-                yamlFile.createSection("UID.customArmor.Helm");
-                yamlFile.createSection("UID.customArmor.ChestPlate");
-                yamlFile.createSection("UID.customArmor.Leggings");
-                yamlFile.createSection("UID.customArmor.Boots");
-                yamlFile.createSection("UID.customArmor.Other");
-
-                yamlFile.createSection("UID.customWeapon.Sword");
-                yamlFile.createSection("UID.customWeapon.Shield");
-                yamlFile.createSection("UID.customWeapon.Axe");
-                yamlFile.createSection("UID.customWeapon.Bow");
-                yamlFile.createSection("UID.customWeapon.Other");
-
-                yamlFile.set("UID.customArmor.Helm", 1);
-                yamlFile.set("UID.customArmor.ChestPlate", 1);
-                yamlFile.set("UID.customArmor.Leggings", 1);
-                yamlFile.set("UID.customArmor.Boots", 1);
-                yamlFile.set("UID.customArmor.Other", 1);
-
-                yamlFile.set("UID.customWeapon.Sword", 1);
-                yamlFile.set("UID.customWeapon.Shield", 1);
-                yamlFile.set("UID.customWeapon.Axe", 1);
-                yamlFile.set("UID.customWeapon.Bow", 1);
-                yamlFile.set("UID.customWeapon.Other", 1);
-                try {
-                    yamlFile.save(file);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
         }
         else{
-            this.exists = true;
+            itemFilesExists = true;
         }
+
+
         //MainCombat.logger.info("Loading UID system.");
 
     }
