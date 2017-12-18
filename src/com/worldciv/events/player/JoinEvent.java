84c0db8f1114461d99b4a811a4195b9ce82b33bb
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2017-12-18 12:27:05 -0800

    minor bug fixes

--- src/com/worldciv/events/player/JoinEvent.java
+++ src/com/worldciv/events/player/JoinEvent.java
@@ -1,5 +1,8 @@
 package com.worldciv.events.player;
 
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.worldciv.the60th.Main;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +11,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import javax.xml.bind.Marshaller;
 
+import static com.worldciv.the60th.Main.getWorldGuard;
+import static com.worldciv.the60th.Main.vision_bypass;
+import static com.worldciv.utility.utilityArrays.visionregion;
+
 public class JoinEvent implements Listener {
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event){
@@ -15,5 +22,17 @@ public class JoinEvent implements Listener {
         player.setMaxHealth(40);
         // SCOREBOARD CREATION //
         Main.getScoreboardManager().setScoreboard(player);
+
+        ApplicableRegionSet set = getWorldGuard().getRegionManager(player.getWorld()).getApplicableRegions(player.getLocation());
+
+        if (!visionregion.contains(player)) {
+
+            for (ProtectedRegion region : set) {
+                if (region.getFlag(vision_bypass) == StateFlag.State.ALLOW) {
+                    visionregion.add(player);
+                }
+            }
+
+        }
     }
 }
