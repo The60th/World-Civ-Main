efd9e2f733b16fe051e855527b6638f789eb93b6
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2018-01-01 12:00:35 -0800

    null fix

--- src/com/worldciv/events/player/CancelEnderpearlEvent.java
+++ src/com/worldciv/events/player/CancelEnderpearlEvent.java
@@ -13,6 +13,14 @@ public class CancelEnderpearlEvent implements Listener {
     public void onEnderpearlToss(PlayerInteractEvent e) {
         Player p = e.getPlayer();
         ItemStack is = e.getItem();
+if (is == null){
+    return;
+}
+
+if (is.getType() == null){
+    return;
+}
+
 
         if (is.getType().equals(Material.ENDER_PEARL)) {
             if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
